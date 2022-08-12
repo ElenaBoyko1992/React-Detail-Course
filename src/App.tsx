07f75077ed81be2b1b6206f30c52de3bee0f8051
ElenaBoyko1992 <Bionka@inbox.ru>
2022-08-12 10:53:15 +0300

    добавление пропсов

--- src/App.tsx
+++ src/App.tsx
@@ -7,23 +7,34 @@ function hello() {
     debugger
     alert('Hello IT-KAMASUTRA')
 }
+
 //hello();
 
 function App() {
     console.log("App rendering")
     return (
         <div>
-            <AppTitle />
-            <Rating/>
-            <Accordion/>
-            <Rating/>
+            <PageTitle title={"This is APP component"}/>
+            <PageTitle title={"My friends"}/>
+            Article 1
+            <Rating value={3}/>
+            <Accordion title={"first Accordion title"} />
+            <Accordion title={"second Accordion title"} />
+            Article 2
+            <Rating value={0}/>
+            <Rating value={1}/>
+            <Rating value={2}/>
+            <Rating value={3}/>
+            <Rating value={4}/>
+            <Rating value={5}/>
         </div>
     );
 }
 
-function AppTitle() {
-    console.log("AppTitle rendering")
-    return <>This is APP component</>
+function PageTitle(props: any) {
+    console.log("PageTitle rendering")
+
+    return <h1>{props.title} </h1>
 }
 
 
