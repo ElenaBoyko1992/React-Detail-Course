c7172859561a13dd0adf1e9ba32219d6990b90c9
ElenaBoyko1992 <Bionka@inbox.ru>
2022-08-29 17:21:35 +0300

    useState learning

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Accordion from "./components/Accordion/Accordion";
 import {Rating} from "./components/Rating/Rating";
+import OnOff from "./components/OnOff/OnOff";
 
 
 function hello() {
@@ -16,16 +17,19 @@ function App(props: any) {
     return (
         <div>
 
-            <Accordion titleValue={"Menu"} collapsed={true}/>
+            <OnOff />
+            <OnOff />
+{/*            <OnOff />
+            <OnOff />*/}
+
+{/*            <Accordion titleValue={"Menu"} collapsed={true}/>
             <Accordion titleValue={"Users"} collapsed={false}/>
             <Rating value={0}/>
             <Rating value={1}/>
             <Rating value={2}/>
             <Rating value={3}/>
             <Rating value={4}/>
-            <Rating value={5}/>
-
-            {/*<OnOff value={true}/>*/}
+            <Rating value={5}/>*/}
 
         </div>
     );
